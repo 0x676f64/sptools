d89f42c458726229161a45823f2b5365e433c381
Joe Rodriguez <oscardiggz@protonmail.com>
2025-01-09 16:54:17 -0600

    Update guest_page.dart

--- lib/pages/guest_page.dart
+++ lib/pages/guest_page.dart
@@ -1,14 +1,78 @@
 import 'package:flutter/material.dart';
 
-class GuestPage extends StatelessWidget {
+class GuestPage extends StatefulWidget {
   const GuestPage({super.key});
 
   @override
+  _GuestPageState createState() => _GuestPageState();
+}
+
+class _GuestPageState extends State<GuestPage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: const Color(0xFF0A0A0A),
       body: Center(
-        child: Text("Guest Page", style: TextStyle(fontSize: 24)),
+        child: Text(
+          'Page: ${_getPageName(_selectedIndex)}',
+          style: const TextStyle(color: Colors.white, fontSize: 24),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color(0xFF0A0A0A),
+        selectedItemColor: const Color(0xFFF26722),
+        unselectedItemColor: Colors.grey,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag),
+            label: 'Products',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Wishlist',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long),
+            label: 'Orders',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.more_horiz),
+            label: 'More',
+          ),
+        ],
       ),
     );
+  }
+
+  String _getPageName(int index) {
+    switch (index) {
+      case 0:
+        return 'Home';
+      case 1:
+        return 'Products';
+      case 2:
+        return 'Wishlist';
+      case 3:
+        return 'Orders';
+      case 4:
+        return 'More';
+      default:
+        return 'Home';
+    }
   }
 }
